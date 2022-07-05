c047515c19de793677efe7c2ccbca8be460d68fc
José Miguel Chavez <jose.chavez.g@usach.cl>
2022-07-04 20:52:09 -0400

    final commit

--- backend/src/main/java/com/tbd/control5/backend/InstitutionModel.java
+++ backend/src/main/java/com/tbd/control5/backend/InstitutionModel.java
@@ -1,17 +1,9 @@
 package com.tbd.control5.backend;
 import org.springframework.data.annotation.Id;
 
-// import javax.validation.constraints.Email;
-// import javax.validation.constraints.NotNull;
-// import javax.validation.constraints.Size;
-
 public class InstitutionModel {
     @Id private String id;
-    // @NotNull(message = "Debe indicar un nombre de Institución")
-    // @Size(min=1, max=200, message="Debe tener al menos 1 caracter")
     private String name;
-    // @NotNull(message = "Debe indicar un email")
-    // @Email(message = "Debe indicar un mail valido")
     private String email;
     private String contact;
 
